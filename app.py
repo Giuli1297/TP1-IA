baf4532a6feb1540fb6cc37e1a0981b243dffe0b
raulgalvan <ramako72@gmail.com>
2022-09-18 16:51:22 -0400

    feat: metodo avaro

--- app.py
+++ app.py
@@ -92,6 +92,43 @@ def vegasTSP(nodo_actual, n_visitados, nodo_inicial, N, co, ruta):
         if solution:
             return solution
 
+def avaroTSP(nodo_actual, n_visitados, nodo_inicial, N, co, ruta):
+    nodos_visitados = n_visitados.copy()
+    fronteras = []
+    froteras_visitadas = []
+    co.append(1)
+    tree_node_number = len(co)
+    for ar in nodo_actual.aristas:
+        if (ar.nodo2 not in nodos_visitados or ar.nodo2 == nodo_inicial) and ar.nodo2 != nodo_actual:
+            fronteras.append(ar.nodo2)
+        if (ar.nodo1 not in nodos_visitados or ar.nodo1 == nodo_inicial) and ar.nodo1 != nodo_actual:
+            fronteras.append(ar.nodo1)
+    nodos_visitados.append(nodo_actual)
+    while True:
+        # print(tree_node_number)
+        # print(str(co) + "Nodo actual: " + nodo_actual.nombre + " - NVisitados: ", end="")
+        # printList(nodos_visitados)
+        # print(" - Frontera: ", end="")
+        # printList(fronteras)
+        # print(" - Frontera V: ", end="")
+        # printList(froteras_visitadas)
+        # print("")
+        if nodo_inicial in fronteras:
+            fronteras.remove(nodo_inicial)
+            if len(nodos_visitados) == N:
+                # print(nodos_visitados, file=sys.stdout)
+                print("x", file=sys.stdout)
+                ruta.extend(nodos_visitados)
+                return nodos_visitados
+        fronterasv2 = [x for x in fronteras if x not in froteras_visitadas]
+        if not fronterasv2:
+            return []
+        target = fronterasv2[0]
+        froteras_visitadas.append(target)
+        solution = avaroTSP(target, nodos_visitados, nodo_inicial, N, co, ruta)
+        if solution:
+            return solution
+
 
 def generateGraph(size, aristas, max_cost):
     if size <= 0 or aristas > (((size * (size - 1)) / 2) - size):
@@ -174,6 +211,8 @@ start = time.time()
 contador = []
 nodosCamin = []
 datos = vegasTSP(nodo_inicial, [], nodo_inicial, N, contador, nodosCamin)
+
+datos = avaroTSP(nodo_inicial, [], nodo_inicial, N, contador, nodosCamin)
 # print(len(contador))
 # print(nodosCamin)
 end = time.time()
